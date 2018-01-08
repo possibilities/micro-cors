ab5ab6fedb74b08084ed562ca86f2f2da4aae609
Mirza Brunjadze <brunjadzem@gmail.com>
2018-01-08 18:29:18 +0400

    Allow additional handler arguments (#27)

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const DEFAULT_ALLOW_HEADERS = [
 
 const DEFAULT_MAX_AGE_SECONDS = 60 * 60 * 24 // 24 hours
 
-const cors = options => handler => (req, res) => {
+const cors = options => handler => (req, res, ...restArgs) => {
   const {
     maxAge,
     origin,
@@ -56,7 +56,7 @@ const cors = options => handler => (req, res) => {
 
   res.setHeader('Access-Control-Allow-Credentials', 'true')
 
-  return handler(req, res)
+  return handler(req, res, ...restArgs)
 }
 
 module.exports = cors
